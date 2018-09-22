922db667e5e73c9108a533f7687c7f25bd2e3dd0
evanstpaul16 <evanstpaul16@OLINLAB204-19A.augustana.edu>
2018-09-22 16:16:33 -0500

    Created base code for classes in dataModel package
    Getters/setters, and a few methods I think will be useful (ie.
    getPositionAtFrame() in AnimalTrack)

--- ProjectBittern/src/dataModel/AnimalTrack.java
+++ ProjectBittern/src/dataModel/AnimalTrack.java
@@ -1,5 +1,32 @@
 package dataModel;
 
-public class AnimalTrack {
+import java.awt.Point;
+import java.util.*;
 
+public class AnimalTrack {
+	//fields
+	private String animalID;
+	private List<TimePoint> positions;
+	
+	//constructor
+	public AnimalTrack(String animalID, List<TimePoint> positions) {
+		this.animalID = animalID;
+		this.positions = positions;
+	}
+	
+	
+	public String getAnimalID() {
+		return animalID;
+	}
+
+
+	public Point getPositionAtFrame(int frameNum) {
+		return positions.get(frameNum).getPt();
+	}
+
+	
+	
+	
+	
+	
 }
--- ProjectBittern/src/dataModel/TimePoint.java
+++ ProjectBittern/src/dataModel/TimePoint.java
@@ -1,5 +1,28 @@
 package dataModel;
 
-public class TimePoint {
+import java.awt.Point;
 
+public class TimePoint {
+	//fields
+	private Point pt;
+	private int frameNum;
+	
+	//constructor
+	public TimePoint(Point pt, int frameNum) {
+		this.pt = pt;
+		this.frameNum = frameNum;
+	}
+	
+
+	public Point getPt() {
+		return pt;
+	}
+
+
+	public int getFrameNum() {
+		return frameNum;
+	}
+
+	
+	
 }
