56cd2f5ac03f74cd4c14e71f030c822bf548977b
evanstpaul16 <evanstpaul16@OLINLAB204-19A.augustana.edu>
2018-09-22 14:49:19 -0500

    Added fields and getDurationinSeconds method to Video class
    Basically just a push test to see if everything was set up properly.

--- ProjectBittern/src/dataModel/Video.java
+++ ProjectBittern/src/dataModel/Video.java
@@ -1,5 +1,22 @@
 package dataModel;
 
-public class Video {
+import java.awt.Rectangle;
 
+public class Video {
+	//fields
+	private double frameRate;
+	private double xPixelsPerCm;
+	private double yPixelsPerCm;
+	private int totalNumFrames;
+	private String filePath;
+	private int startFrameNum;
+	private int endFrameNum;
+	private Rectangle arenaBounds;
+	
+	//constructor coming soon 
+	
+	
+	public double getDurationInSeconds() {
+		return totalNumFrames / frameRate;
+	}
 }
