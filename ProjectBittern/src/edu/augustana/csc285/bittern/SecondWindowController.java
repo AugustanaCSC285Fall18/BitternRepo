bad8f43eb492f068f1de10c80a17d3f78a0d1938
carpe <carpe@10.100.14.54>
2018-09-22 15:06:49 -0500

    Changed layout of the second window.

--- ProjectBittern/src/edu/augustana/csc285/bittern/SecondWindowController.java
+++ ProjectBittern/src/edu/augustana/csc285/bittern/SecondWindowController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package edu.augustana.csc285.bittern;
 
 import java.io.ByteArrayInputStream;
@@ -10,6 +9,8 @@ import org.opencv.videoio.VideoCapture;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -20,6 +21,13 @@ public class SecondWindowController {
 	
 	@FXML private VideoCapture vidCap = new VideoCapture();
 	
+	@FXML private Button setStart;
+	@FXML private Button setEnd;
+	@FXML private Button confirm;
+	
+	@FXML private TextField startTime;
+	@FXML private TextField endTime;
+	
 	@FXML public void initialize() {	
 	}
 	
@@ -28,18 +36,17 @@ public class SecondWindowController {
 	
 	
 	
-	public void video() {
-		Mat frame = new Mat();
-		vidCap.read(frame);
-		MatOfByte buffer = new MatOfByte();
-		Imgcodecs.imencode(".png", frame, buffer);
-		Image toShow = new Image(new ByteArrayInputStream(buffer.toArray()));
-		Platform.runLater(new Runnable() {
-			@Override public void run() { 
-				videoView.setImage(toShow); }
-		});
-	}
+//	public void video() {
+//		Mat frame = new Mat();
+//		vidCap.read(frame);
+//		MatOfByte buffer = new MatOfByte();
+//		Imgcodecs.imencode(".png", frame, buffer);
+//		Image toShow = new Image(new ByteArrayInputStream(buffer.toArray()));
+//		Platform.runLater(new Runnable() {
+//			@Override public void run() { 
+//				videoView.setImage(toShow); }
+//		});
+//	}
 	
 	
 }
-
