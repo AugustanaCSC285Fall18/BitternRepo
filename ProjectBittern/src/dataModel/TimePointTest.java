8a1ff73a24d705b39d4db14244b5536ba6de08e5
chitran17 <chitran17@OLINLAB204-11.augustana.edu>
2018-10-09 11:16:12 -0500

    working on timepoint unit test for lab

--- ProjectBittern/src/dataModel/TimePointTest.java
+++ ProjectBittern/src/dataModel/TimePointTest.java
@@ -1,3 +1,4 @@
+
 package dataModel;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,20 +9,31 @@ import dataModel.TimePoint.*;
 
 class TimePointTest {
 	
-	AnimalTrack chick1 = new AnimalTrack("howdy");
-	
-	
 	TimePoint a = new TimePoint(0, 0, 19);
 	TimePoint b = new TimePoint(4, 3, 19);
 	TimePoint c = new TimePoint(1, 2, 3);
 	
 	@Test
-	public void test() {
-		chick1.add(a);
-		a.setX(4);
-		a.setY(3);
-		
-		assertEquals(b, chick1.getTimePointAtTime(19));
+	public void testForGetX() {
+		assertEquals(0, a.getX());
+		assertEquals(4, b.getX());
+		assertEquals(1, c.getX());
 	}
+	
+	@Test
+	public void testForGetY() {
+		assertEquals(0, a.getY());
+		assertEquals(3, b.getY());
+		assertEquals(2, c.getY());
+	}
+	
+	@Test
+	public void testForGetFrameNum() {
+		assertEquals(19, a.getFrameNum());
+		assertEquals(19, b.getFrameNum());
+		assertEquals(3, c.getFrameNum());
+	}
+	
+	
 
 }
