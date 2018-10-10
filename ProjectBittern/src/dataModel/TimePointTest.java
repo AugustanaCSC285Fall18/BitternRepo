513abddfffc4437a09116465dcc97b7379a57b68
carpe <carpe@172.16.118.253>
2018-10-10 08:39:38 -0500

    Attempting to get TimePointTest to work.

--- ProjectBittern/src/dataModel/TimePointTest.java
+++ ProjectBittern/src/dataModel/TimePointTest.java
@@ -15,11 +15,16 @@ class TimePointTest {
 	TimePoint b = new TimePoint(4, 3, 19);
 	TimePoint c = new TimePoint(1, 2, 3);
 	
+	/*
+	 * This method doesn't work properly yet, need to fix.
+	 */
 	@Test
 	public void test() {
-		chick1.add(a);
+//		chick1.add(a);
 		a.setX(4);
 		a.setY(3);
+		chick1.add(a);
+//		chick1.add(c);
 		
 		assertEquals(b, chick1.getTimePointAtTime(19));
 	}
