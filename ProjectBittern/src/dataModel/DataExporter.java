4821c85a24572d7fe014bf7d7035d3b33f03065a
danielleosazuwa16 <danielleosazuwa16@augustana.edu>
2018-10-28 20:36:43 -0500

    added total distance per chick to dataExporter

--- ProjectBittern/src/dataModel/DataExporter.java
+++ ProjectBittern/src/dataModel/DataExporter.java
@@ -27,11 +27,12 @@ public class DataExporter {
 		try {
 			for (AnimalTrack track : project.getTracks()) {
 				fWriter.append(track.getID());
-				fWriter.append("\n");
-				fWriter.append("Time, X Positions, Y Positions, Distance From Origin (cm)");
-				fWriter.append("\n");
+				fWriter.append("\nTime, X Positions, Y Positions, Distance From Origin (cm)\n");
 				fWriter.append(getResult(track, project.getVideo()));
 			}
+			fWriter.append("\n");
+			fWriter.append(getTotalDistanceOutput(project.getTracks(), project.getVideo()));
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -65,10 +66,22 @@ public class DataExporter {
 					+ (int) currentPoint.getY() + ", "
 					+ (int) currentPoint.getDistanceTo(origin) + "\n";
 		}
+
+		return output;
+	}
+	
+	private static String getTotalDistanceOutput(List<AnimalTrack> tracks, Video video) {
+		String output = "Chick Name, Total Distance\n";
+		double totalDistance = 0;
+		for (AnimalTrack track : tracks) {
+			List<TimePoint> convertedPositions = getConvertedPositions(track.getPositions(),video);
+			totalDistance = getTotalDistance(convertedPositions, video);
+			output += track.getID() + "," + (int)totalDistance + "\n";
+		}
 		
 		return output;
 	}
-
+	
 	/**
 	 * adjusts the given AnimalTrack's list of TimePoints relative to the given origin point
 	 * @param track the AnimalTrack object containing the list of TimePoints to be adjusted
@@ -80,17 +93,37 @@ public class DataExporter {
 		List<TimePoint> calibratedTimePoints = new ArrayList<>();
 		
 		for (int i = 0; i < track.getPositions().size(); i++) {
-			TimePoint currentPoint = track.getPositions().get(i);
-			double newX = currentPoint.getX() / video.getXPixelsPerCm();
-			double newY = currentPoint.getY() / video.getYPixelsPerCm();
-				
-			newX -= origin.getX();
-			newY -= origin.getY();
-			calibratedTimePoints.add(new TimePoint(newX,newY,currentPoint.getFrameNum()));
+			TimePoint currentPoint = getConvertedTimePoint(track.getPositions().get(i), video);
+			currentPoint.setX(currentPoint.getX() - origin.getX());
+			currentPoint.setY(currentPoint.getY() - origin.getY());
+			calibratedTimePoints.add(currentPoint);
 		}
 
 		return calibratedTimePoints;
 	}
 
+	private static double getTotalDistance(List<TimePoint> adjustedPositions, Video video) {
+		double totalDistance = 0;
+		for (int i = 1; i < adjustedPositions.size(); i++) {
+			TimePoint currentPoint = adjustedPositions.get(i);
+			totalDistance += currentPoint.getDistanceTo(adjustedPositions.get(i-1));
+		}
+		return totalDistance;
+	}
+
+	private static TimePoint getConvertedTimePoint(TimePoint point, Video video) {
+		double newX = point.getX() / video.getXPixelsPerCm();
+		double newY = point.getY() / video.getYPixelsPerCm();
+		TimePoint convertedPoint = new TimePoint(newX, newY, point.getFrameNum());
+		return convertedPoint;
+	}
 	
+	private static List<TimePoint> getConvertedPositions(List<TimePoint> positions, Video video) {
+		List<TimePoint> convertedPositions = new ArrayList<>();
+		for (TimePoint point : positions) {
+			convertedPositions.add(getConvertedTimePoint(point, video));
+		}
+		return convertedPositions;
+	}
+
 }
